a84a97d098060c3fedfabdd7284455f806a9675f
Michal Horejsek <michal.horejsek@nic.cz>
2017-10-19 15:10:06 +0200

    Release 1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if sys.version_info < (3, 0):
 
 setup(
     name='haas-proxy',
-    version='1.0',
+    version='1.1',
     packages=[
         'haas_proxy',
         'haas_proxy.twisted.plugins',
